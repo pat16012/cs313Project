b83a5811351c90a0ed4f4755fea3fdcd9b99370b
Justin Paton <pat16012@byui.edu>
2019-02-25 16:49:44 -0700

    s

--- web/signup.php
+++ web/signup.php
@@ -1,69 +1,27 @@
 <?php 
-/* Main page with two forms: sign up and log in */
+/* sign up and registration */
 require_once 'db.php';
 $db = get_db();
 session_start();
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Sign-Up/Login Form</title>
-  <?php include 'css/css.html'; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign-up</title>
+    <?php include 'css/css.html'; ?>
 </head>
-
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-    if (isset($_POST['login'])) { //user logging in
-
-        require 'login.php';
-        
-    }
-    
-    elseif (isset($_POST['register'])) { //user registering
-        
-        require 'register.php';
-        
-    }
-}
-?>
 <body>
-  <div class="form">
+<div class="form">
       
       <ul class="tab-group">
         <li class="tab"><a href="signup.php">Sign Up</a></li>
-        <li class="tab active"><a href="#login">Log In</a></li>
+        <li class="tab active"><a href="index.php">Log In</a></li>
       </ul>
-      
-      <div class="tab-content">
-
-         <div id="login">   
-          <h1>Welcome</h1>
-          
-          <form action="index.php" method="post" autocomplete="off">
-          
-            <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email" required autocomplete="off" name="email"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password" required autocomplete="off" name="password"/>
-          </div>
-          
-          <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
-          
-          <button class="button button-block" name="login" >Log In</button>
-          
-          </form>
-
-        </div>
-        <div id="signup">   
+<div id="signup">   
           <h1>Sign Up for Free</h1>
           
           <form action="index.php" method="post" autocomplete="off">
@@ -105,8 +63,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         </div>  
         
       </div>
-</div> <!-- /form -->
- <!-- <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+</div> <!-- /form 
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <script src="js/index.js"></script>
 -->
