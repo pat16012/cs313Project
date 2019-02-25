f5686ba96ab946eda0d3af9dd1359830d57ce20f
Justin Paton <pat16012@byui.edu>
2019-02-25 15:30:14 -0700

    j

--- web/index.php
+++ web/index.php
@@ -107,9 +107,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       </div><!-- tab-content -->
       
 </div> <!-- /form -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <!--<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-    <script src="js/index.js"></script>
+    <script src="js/index.js"></script>-->
 
 </body>
 </html>
--- web/profile.php
+++ web/profile.php
@@ -64,8 +64,8 @@ else {
 
     </div>
     
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<!--<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src="js/index.js"></script>
-
+        -->
 </body>
 </html>
