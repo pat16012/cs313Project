3c5379ccb8eaaee18c7978675b749b819f52543a
Justin Paton <pat16012@byui.edu>
2019-02-25 14:20:09 -0700

    a

--- web/index.php
+++ web/index.php
@@ -30,14 +30,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   <div class="form">
       
       <ul class="tab-group">
-        <li class="tab"><a href="#signup">Sign Up</a></li>
+        <li class="tab"><a href="signup.php">Sign Up</a></li>
         <li class="tab active"><a href="#login">Log In</a></li>
       </ul>
       
       <div class="tab-content">
 
          <div id="login">   
-          <h1>Welcome!</h1>
+          <h1>Welcome</h1>
           
           <form action="index.php" method="post" autocomplete="off">
           
@@ -62,49 +62,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
           </form>
 
         </div>
-          
-        <div id="signup">   
-          <h1>Sign Up for Free</h1>
-          
-          <form action="index.php" method="post" autocomplete="off">
-          
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" name='firstname' />
-            </div>
-        
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off" name='lastname' />
-            </div>
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off" name='email' />
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off" name='password'/>
-          </div>
-          
-          <button type="submit" class="button button-block" name="register" >Register</button>
-          
-          </form>
-
-        </div>  
-        
-      </div><!-- tab-content -->
       
 </div> <!-- /form -->
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
---
+++ web/signup.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign-up</title>
+    <?php include 'css/css.html'; ?>
+</head>
+<body>
+<div id="signup">   
+          <h1>Sign Up for Free</h1>
+          
+          <form action="index.php" method="post" autocomplete="off">
+          
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                First Name<span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off" name='firstname' />
+            </div>
+        
+            <div class="field-wrap">
+              <label>
+                Last Name<span class="req">*</span>
+              </label>
+              <input type="text"required autocomplete="off" name='lastname' />
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off" name='email' />
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Set A Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off" name='password'/>
+          </div>
+          
+          <button type="submit" class="button button-block" name="register" >Register</button>
+          
+          </form>
+
+        </div>  
+        
+      </div>
+
+      </div> <!-- /form -->
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+</body>
+</html>
