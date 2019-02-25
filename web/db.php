a427b926e4bc007824b5154131f141e9c34e51d2
Justin Paton <pat16012@byui.edu>
2019-02-25 13:03:59 -0700

    DB Connection update

--- web/db.php
+++ web/db.php
@@ -1,7 +1,28 @@
 <?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'mypass123';
-$db = 'accounts';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+function get_db() {
+	$db = NULL;
+	try {
+		
+		$dbUrl = getenv('DATABASE_URL');
+		
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+	return $db;
+}
