9eee1a9cb42c317f2e764127dedd97a7688e236d
Justin Paton <pat16012@byui.edu>
2019-02-26 16:43:50 -0700

    s

--- web/verify.php
+++ web/verify.php
@@ -14,18 +14,7 @@ $query = "SELECT * FROM users WHERE email='$email'";
 $result = pg_query($db,$query);
 $resultData = pg_fetch_array($result,0);
 
-
-
 echo("This is the result before if: $resultData <br>");
-print_r($resultData);
-
-
-
-$user = pg_fetch_all($result, PGSQL_BOTH);
-
-echo("Data [0]: $user");
-echo("Data [0]: $user[0]");
-print_r($user);
 
 if($resultData[0] != $email){
     $_SESSION['message'] = "User with that email doesn't exist!";
