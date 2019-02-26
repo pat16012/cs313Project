cfea8fc2f2f772148f26ecf5fccedd3715cf31d2
Justin Paton <pat16012@byui.edu>
2019-02-26 12:15:53 -0700

    update

--- web/db.php
+++ web/db.php
@@ -3,7 +3,7 @@ function get_db() {
 	$db = NULL;
 	try {
 		
-		$dbUrl = getenv('JAWSDB_URL');
+		$dbUrl = getenv('DATABASE_URL');
 		
 		// Get the various parts of the DB Connection from the URL
 		$dbopts = parse_url($dbUrl);
@@ -13,7 +13,7 @@ function get_db() {
 		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
 		// Create the PDO connection
-		$db = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db = new PDO("postgres://host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
