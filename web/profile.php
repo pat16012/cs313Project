547575b7ab0ec666d17bed63a6a9b0e46e8c1e3e
Justin Paton <pat16012@byui.edu>
2019-02-26 20:34:50 -0700

    jtp

--- web/profile.php
+++ web/profile.php
@@ -28,28 +28,11 @@ else {
 </head>
 <body>
   <div class="form">
-
-          <h1>Welcome</h1>
-          
-          <p>
-          <?php 
-     
-          // Display message about account verification link only once
-          if ( isset($_SESSION['message']) )
-          {
-              echo $_SESSION['message'];
-              
-              // Don't annoy the user with more messages upon page refresh
-              unset( $_SESSION['message'] );
-          }
-          
-          ?>
-          </p>
           
           <h2><?php echo $first_name.' '.$last_name; ?></h2>
           <p><?= $email ?></p>
           
-          <a href="logout.php"><button class="button button-block" name="logout"/>Log Out</button></a>
+          <a href="logout.php"><button class="button button-block" name="logout">Log Out</button></a>
 
     </div>
     
