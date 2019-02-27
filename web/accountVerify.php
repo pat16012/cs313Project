dc792d86503a7b600d5fa5af3c3cd0e645186eb2
Justin Paton <pat16012@byui.edu>
2019-02-26 21:30:11 -0700

    added account verification

--- web/accountVerify.php
+++ web/accountVerify.php
@@ -4,19 +4,22 @@
 */
 
 require ('db.php');
+$db = get_db();
 session_start();
 
 
 // Make sure email and hash variables aren't empty
 if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']))
 {
-    $email = $mysqli->escape_string($_GET['email']); 
-    $hash = $mysqli->escape_string($_GET['hash']); 
+    $email = pg_escape_string($_GET['email']);  
+    $hash = pg_escape_string($_GET['hash']); 
     
     // Select user with matching email and hash, who hasn't verified their account yet (active = 0)
-    $result = $mysqli->query("SELECT * FROM users WHERE email='$email' AND hash='$hash' AND active='0'");
+    $query = "SELECT * FROM users WHERE email='$email' AND hash='$hash' AND active='FALSE'";
+    $result = pg_query($db,$query);
+    $resultData = pg_fetch_all($result,PGSQL_BOTH);
 
-    if ( $result->num_rows == 0 )
+    if ( $resultData[0][5] == 'TRUE' )
     { 
         $_SESSION['message'] = "Account has already been activated or the URL is invalid!";
 
@@ -26,10 +29,15 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
         $_SESSION['message'] = "Your account has been activated!";
         
         // Set the user status to active (active = 1)
-        $mysqli->query("UPDATE users SET active='1' WHERE email='$email'") or die($mysqli->error);
-        $_SESSION['active'] = 1;
-        
-        header("location: success.php");
+        $sql = "UPDATE users SET active='TRUE' WHERE email='$email'";
+        if( pg_query($db, $sql)){
+            $_SESSION['active'] = 1;
+            header("location: success.php");
+        }
+        else{
+            $_SESSION['message'] = "Activation Failed, Please try again";
+            header("location: error.php");
+        }
     }
 }
 else {
