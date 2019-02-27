859b97138ed2db8eead1cdf69256d3774c9b1b79
Justin Paton <pat16012@byui.edu>
2019-02-26 20:22:14 -0700

    update

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome</title>
+    <?php include 'css/css.html'; ?>
 </head>
 
 
