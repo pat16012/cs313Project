da703fd0e8a040d8c25ab1a9f3ec7f8b1807dd9e
Justin Paton <pat16012@byui.edu>
2019-02-26 17:00:43 -0700

    fd

--- web/verify.php
+++ web/verify.php
@@ -6,28 +6,17 @@ session_start();
 // Escape to protect against SQL injection
 $email = pg_escape_string($_POST['email']);
 $password = pg_escape_string($_POST['password']);
- 
-echo("THis is the post e-mail : $email");
 
 // Check to see if the email already exists
 $query = "SELECT * FROM users WHERE email='$email'";
 $result = pg_query($db,$query);
 $resultData = pg_fetch_all($result,PGSQL_BOTH);
 
-
-
-//echo("This is the result before if: $resultData <br>");
-
-
-
-
-$user = pg_fetch_all($result, PGSQL_BOTH);
-
 //echo("Datauser [0]: $user");
-//echo("Datauser [0]: $user[0]");
-print_r($user);
+echo("Datauser [0]: " . $user[0][3]);
+print_r($resultData);
 
-if($resultData[0] != $email){
+if($resultData[0][3] != $email){
     $_SESSION['message'] = "User with that email doesn't exist!";
     echo("Email not used before");
    // header("location: error.php");
