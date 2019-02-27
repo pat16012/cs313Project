9c75288a879ae6af0555a6bf96f37a84b2b8cc69
Justin Paton <pat16012@byui.edu>
2019-02-26 22:21:03 -0700

    dsd

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 require ('db.php');
 session_start();
+$signup = "signup.php";
+$login = "login.php";
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +20,8 @@ session_start();
 
 <div class="form">
 <ul class="tab-group">
-        <li class="tab active"><a href="signup.php">Sign Up</a></li>
-        <li class="tab active"><a href="login.php">Log In</a></li>
+        <li class="tab active"><a href=<?php $signup ?>>Sign Up</a></li>
+        <li class="tab active"><a href=<?php $login ?>>Log In</a></li>
       </ul>
 </div>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
