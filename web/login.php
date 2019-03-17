2143b4ab4c11644dc69e7447dcb25a0d9a439747
Justin Paton <pat16012@byui.edu>
2019-03-16 22:02:35 -0600

    update

--- web/login.php
+++ web/login.php
@@ -13,10 +13,10 @@ session_start();
     <?php include 'css/css.html'; ?>
 </head>
 <body>
-<img  src="img/famjamlogo.jpg">
+
 
 <div class="form">
-      
+<img  src="img/famjamlogo.jpg">
       <ul class="tab-group">
         <li class="tab active"><a href="signup.php">Sign Up</a></li>
         <li class="tab active"><a href="login.php">Log In</a></li>
