a7e565900eb18b0db774968186d97a23ec00abb9
Justin Paton <pat16012@byui.edu>
2019-03-16 19:58:21 -0600

    updated index

--- web/addFam.php
+++ web/addFam.php
@@ -1,6 +1,6 @@
 <?php include('loggedInCheck.php')?>
 <?php include('header.php')?>
-<?php include('rightMenu.php')?>
+<?php include('leftMenu.php')?>
 
 <div class="col-2 col-s-2">
       <h1>Invite a Family Member</h1>
