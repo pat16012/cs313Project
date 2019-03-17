5dbccacffc7d2c4943b5ed7bee8dce006c26f799
Justin Paton <pat16012@byui.edu>
2019-03-16 19:50:52 -0600

    updated index

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require ('db.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <?php include 'css/css.html'; ?>
+</head>
+<body>
+<div class="form">
+<img class="imgfam" src="web/img/famjamlogo.jpg">
+<div>
+<img class="imgfam" src="web/img/famjam.jpg">
+<h2>Please Choose a Selection</h2>
+<ul class="tab-group">
+        <li class="tab active"><a href="web/signup.php">Sign Up</a></li>
+        <li class="tab active"><a href="web/login.php">Log In</a></li>
+      </ul>
+</>
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/index.js"></script>
+    
+</body>
+</html>
--- web/success.php
+++ web/success.php
@@ -20,7 +20,7 @@ session_start();
     endif;
     ?>
     </p>
-    <a href="index.php"><button class="button button-block">Home</button></a>
+    <a href="/index.php"><button class="button button-block">Home</button></a>
 </div>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src="js/index.js"></script>
